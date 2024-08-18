244f51c40c9762169e7161db839b9995e8b936d6
yanzhang li <yanzhanglee@outlook.com>
2024-08-18 23:39:09 +0100

    Modified content in user_posting_emulation.py to download tables and save to .csv file

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,9 +1,7 @@
-import requests
+import yaml
+import pandas as pd
 from time import sleep
 import random
-from multiprocessing import Process
-import boto3
-import json
 import sqlalchemy
 from sqlalchemy import text
 
@@ -13,57 +11,44 @@ random.seed(100)
 
 class AWSDBConnector:
 
-    def __init__(self):
-
-        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
-        self.USER = 'project_user'
-        self.PASSWORD = ':t%;yCY3Yjg'
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
+    def __init__(self, creds_file='db_creds.yaml'):
+        # Load database credentials from the YAML file
+        with open(creds_file, 'r') as file:
+            creds = yaml.safe_load(file)
         
+        # Assign the credentials to instance variables
+        self.HOST = creds['db']['HOST']
+        self.USER = creds['db']['USER']
+        self.PASSWORD = creds['db']['PASSWORD']
+        self.DATABASE = creds['db']['DATABASE']
+        self.PORT = creds['db'].get('PORT', 3306)
+
     def create_db_connector(self):
+        # Create the SQLAlchemy engine using the credentials
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
 
-
+# Instantiate the connector
 new_connector = AWSDBConnector()
 
 
-def run_infinite_post_data_loop():
-    while True:
-        sleep(random.randrange(0, 2))
-        random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
+def save_table_to_csv(table_name, engine, file_name):
+    # Fetch all data from the specified table
+    query = text(f"SELECT * FROM {table_name}")
+    with engine.connect() as connection:
+        df = pd.read_sql_query(query, connection)
+        # Save the data to a CSV file
+        df.to_csv(file_name, index=False)
+        print(f"{table_name} saved to {file_name}")
 
-        with engine.connect() as connection:
+def run_post_data_loop():
+    engine = new_connector.create_db_connector()
 
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
-
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
-            
-            for row in geo_selected_row:
-                geo_result = dict(row._mapping)
-
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
-            
-            for row in user_selected_row:
-                user_result = dict(row._mapping)
-            
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
-
+    # Save the tables as CSV files
+    save_table_to_csv('pinterest_data', engine, 'pinterest_data.csv')
+    save_table_to_csv('geolocation_data', engine, 'geolocation_data.csv')
+    save_table_to_csv('user_data', engine, 'user_data.csv')
 
 if __name__ == "__main__":
-    run_infinite_post_data_loop()
-    print('Working')
-    
-    
-
-
+    run_post_data_loop()
+    print('CSV files have been created.')
