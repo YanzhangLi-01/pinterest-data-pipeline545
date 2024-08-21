6b0c9ce94ec309877829f043a247283a47d37f66
yanzhang li <yanzhanglee@outlook.com>
2024-08-21 21:20:13 +0100

    Modify user_posting_emulation.py to send data to kafka topics

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,5 +1,6 @@
 import yaml
-import pandas as pd
+import json
+import requests
 from time import sleep
 import random
 import sqlalchemy
@@ -31,24 +32,75 @@ class AWSDBConnector:
 # Instantiate the connector
 new_connector = AWSDBConnector()
 
+def run_infinite_post_data_loop():
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
 
-def save_table_to_csv(table_name, engine, file_name):
-    # Fetch all data from the specified table
-    query = text(f"SELECT * FROM {table_name}")
-    with engine.connect() as connection:
-        df = pd.read_sql_query(query, connection)
-        # Save the data to a CSV file
-        df.to_csv(file_name, index=False)
-        print(f"{table_name} saved to {file_name}")
+        with engine.connect() as connection:
 
-def run_post_data_loop():
-    engine = new_connector.create_db_connector()
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
 
-    # Save the tables as CSV files
-    save_table_to_csv('pinterest_data', engine, 'pinterest_data.csv')
-    save_table_to_csv('geolocation_data', engine, 'geolocation_data.csv')
-    save_table_to_csv('user_data', engine, 'user_data.csv')
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+            
+            print(pin_result)
+            print(geo_result)
+            print(user_result)
+
+            # Define API Invoke URL for topics
+            pin_invoke_url = "https://4m0a2wpt0f.execute-api.us-east-1.amazonaws.com/dev/topics/12c7b456b441.pin"
+            geo_invoke_url = "https://4m0a2wpt0f.execute-api.us-east-1.amazonaws.com/dev/topics/12c7b456b441.geo"
+            user_invoke_url = "https://4m0a2wpt0f.execute-api.us-east-1.amazonaws.com/dev/topics/12c7b456b441.user"
+
+            # Prepare data for sending
+            pin_payload = json.dumps({
+                "records": [
+                    {
+                        "value": pin_result
+                    }
+                ]
+            }, default=str)
+
+            geo_payload = json.dumps({
+                "records": [
+                    {
+                        "value": geo_result
+                    }
+                ]
+            }, default=str)
+
+            user_payload = json.dumps({
+                "records": [
+                    {
+                        "value": user_result
+                    }
+                ]
+            }, default=str)
+
+            # Send data to the Kafka topics using API Invoke URL
+            headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+            pin_response = requests.request("POST", pin_invoke_url, headers=headers, data=pin_payload)
+            gro_response = requests.request("POST", geo_invoke_url, headers=headers, data=geo_payload)
+            user_response = requests.request("POST", user_invoke_url, headers=headers, data=user_payload)
+            print(pin_response.status_code)
+            print(gro_response.status_code)
+            print(user_response.status_code)
 
 if __name__ == "__main__":
-    run_post_data_loop()
-    print('CSV files have been created.')
+    run_infinite_post_data_loop()
+    print('Working')
